6a8b0585dd80ee6f84a10d8d70a157f6221f9d44
syadinaafifah <102166424+syadinaafifah@users.noreply.github.com>
2023-12-06 13:40:35 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:tokokita/ui/registrasi_page.dart';
+import 'package:tokokita/helpers/user_info.dart';
+import 'package:tokokita/ui/login_page.dart';
+import 'package:tokokita/ui/produk_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Widget page = const CircularProgressIndicator();
+
+  @override
+  void initState() {
+    super.initState();
+    isLogin();
+  }
+
+  void isLogin() async {
+    var token = await UserInfo().getToken();
+    if (token != null) {
+      setState(() {
+        page = const ProdukPage();
+      });
+    } else {
+      setState(() {
+        page = const LoginPage();
+      });
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Toko Kita',
+      debugShowCheckedModeBanner: false,
+      home: page,
+    );
+  }
+}
